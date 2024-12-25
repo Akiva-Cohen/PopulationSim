0ac0ccf958baafb30ab72aa9c0092c3d0c6c65a9
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-25 18:17:55 -0500

    did a whole ton

---
+++ src/IntField.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import javax.swing.text.*;
+public class IntField extends JFormattedTextField {
+    public IntField(int start, int min) {
+        super();
+        NumberFormatter inter = new NumberFormatter();
+        inter.setValueClass(Integer.class);
+        inter.setAllowsInvalid(false);
+        inter.setMinimum(min);
+        setFormatter(inter);
+        setText(Integer.toString(start));
+    }
+    public int getNumber() {
+        return (int)getValue();
+    }
+}
--- src/Run.java
+++ src/Run.java
@@ -1,9 +1,7 @@
 import java.awt.event.ActionListener;
 import java.awt.event.*;
 import java.awt.*;
-
 import javax.swing.*;
-
 
 public class Run {
     int factor;
@@ -15,9 +13,15 @@ public class Run {
     JLabel turnF;
     int turns;
     boolean popWarn;
-    JTextField times;
+    IntField times;
+    IntField delay;
     JPanel inP;
+    boolean counting;
+    boolean running;
+    JButton start;
     public Run(int adultSpan, int childSpan, int adults, int children, int factor) {
+        counting = false;
+        running = false;
         popWarn = true;
         adultArr = new int[adultSpan];
         childArr = new int[childSpan];
@@ -44,8 +48,8 @@ public class Run {
         this.children = new JLabel(Integer.toString(children) + " Smolhajar");
         next = new JButton();
         inP = new JPanel();
-        times = new JTextField("1");
-        inP.add(new JLabel("Simulate "));
+        times = new IntField(1, 1);
+        inP.add(new JLabel("Simulate"));
         inP.add(times);
         inP.add(new JLabel(" Turns"));
         inP.setOpaque(false);
@@ -53,24 +57,37 @@ public class Run {
         next.add(inP);
         turns = 0;
         turnF = new JLabel("0 Turns");
+        delay = new IntField(10,1);
+        start = new JButton("Start/Stop");
     }
     public void run() {
         JFrame main = new JFrame("Population sim");
         main.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        JPanel panel = new JPanel(new GridLayout(3,1));
+        JPanel panel = new JPanel(new GridLayout(2,1));
         JPanel top = new JPanel(new GridLayout(1,2));
         top.add(new JLabel(new ImageIcon(new ImageIcon("app/blahaj.png").getImage().getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH))));
         top.add(new JLabel(new ImageIcon(new ImageIcon("app/smolhaj.png").getImage().getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH))));
         panel.add(top);
-        JPanel middle = new JPanel(new GridLayout(2, 2));
+        JPanel middle = new JPanel(new GridLayout(3, 2));
         middle.add(adults);
         middle.add(children);
         middle.add(next);
         middle.add(turnF);
+        JPanel delayP = new JPanel(new GridBagLayout());
+        delayP.add(new JLabel("Delay:  "));
+        delayP.add(delay);
+        delayP.add(new JLabel("ms"));
+        middle.add(delayP);
+        //middle.add(start);
         panel.add(middle);
         main.add(panel);
         main.pack();
         main.setVisible(true);
+        delay.getDocument().addDocumentListener(new DoDoc() {
+            public void updater() {
+                delayP.validate();
+            }
+        });
         times.getDocument().addDocumentListener(new DoDoc() {
             public void updater() {
                 next.validate();
@@ -78,15 +95,52 @@ public class Run {
         });
         next.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (incrementAndCheck() && popWarn) {
-                    if (SetupPop.stableMode(main)) {
-                        main.dispose();
-                    } else {
-                        popWarn = false;
-                    }
+                if (!running) {
+                    counting = true;
+                    pressResult(main);
                 }
             }
         });
+
+    }
+
+    public void pressResult(JFrame main) {
+        int delayTime = delay.getNumber();
+        ping(main);
+        Timer timer = new Timer(delayTime, null);
+        timer.addActionListener(new ActionListener() {
+            int count = times.getNumber() - 1;
+            public void actionPerformed(ActionEvent e) {
+                if (count <= 0) {
+                    timer.stop();
+                    counting = false;
+                    System.out.println("done counting");
+                } else {
+                    if (ping(main)) {
+                        timer.stop();
+                    }
+                }
+                count--;
+            }
+        });
+        timer.start();
+    }
+
+    public boolean ping(JFrame main) {
+        if (popWarn) {
+            if (incrementAndCheck()) {
+                if (SetupPop.stableMode(main)) {
+                    main.dispose();
+                    return true;
+                } else {
+                    popWarn = false;
+                }
+            }
+        } else {
+            turns++;
+            turnF.setText(Integer.toString(turns) + " Turns");
+        }
+        return false;
     }
     public boolean incrementAndCheck() {
         int[] previousChildren = childArr;
